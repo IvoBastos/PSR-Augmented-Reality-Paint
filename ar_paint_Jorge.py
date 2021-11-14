b1c5e5ddbfa86b0978a237c3ee53c5ca2838b85e
analtino <analtino.martinho@ua.pt>
2021-11-14 20:44:12 +0000

    Refiz a funcionalidade de desenhar circulos e retanguloscom eventos do MOUSE

--- ar_paint_Jorge.py
+++ ar_paint_Jorge.py
@@ -2,7 +2,10 @@
 
 import argparse
 import json
+import math
 import time
+from functools import partial
+
 import cv2
 import numpy as np
 from cv2 import FONT_ITALIC, LINE_8
@@ -13,6 +16,58 @@ ranges_pcss = {"b": {"min": 100, "max": 256},
                "g": {"min": 100, "max": 256},
                "r": {"min": 100, "max": 256},
                }
+
+drawing = False # true if mouse is pressed
+# mode = str('rectangle') # if 'rectangle', draw rectangle.
+ix,iy = -1,-1
+
+# create a white image background dim 600*400
+background = np.zeros((422, 750, 3), np.uint8)
+background.fill(255)
+
+
+def shape(event,x,y,flags,params,mode):
+    global ix,iy,drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        #value of variable draw will be set to True, when you press DOWN left mouse button
+        drawing = True
+        #mouse location is captured here
+        ix,iy=x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+      #Dragging the mouse at this junture
+      if drawing == True:
+        if mode == 'rectangle':
+            #If draw is True then it means you've cliked on the left mouse button
+            #Here we will draw a rectangle from previos position to the x,y where the mouse is currently located
+            cv2.rectangle(background, (ix, iy), (x, y), (0, 255, 0), 3)
+            # Nas ultimas cordenadas devo desenhar um retangulo da cor do fundo
+            a = x
+            b = y
+            if a != x | b != y:
+                cv2.rectangle(background, (ix, iy), (x, y), (255, 255, 255), -1)
+        if mode == 'circle':
+            radius=math.pow(((math.pow(x,2)-math.pow(ix,2))+(math.pow(y,2)-math.pow(iy,2))),1/2)
+            cv2.circle(background,(ix, iy), int(radius), (0, 0, 255),3)
+            a = x
+            b = y
+            if a != x | b != y:
+                radius = math.pow(((math.pow(x, 2) - math.pow(ix, 2)) + (math.pow(y, 2) - math.pow(iy, 2))),1/2)
+                cv2.circle(background,(ix, iy), int(radius), (255, 255, 255), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+     drawing = False
+     if mode == 'rectangle':
+        #As soon as you release the mouse button, variable draw will be set as False
+        #Here we are completing to draw the rectangle on image window
+        # background=clone
+        cv2.rectangle(background, (ix, iy), (x, y), (0, 255, 0), 2)
+
+     if mode == 'circle':
+         radius = math.pow(((math.pow(x, 2) - math.pow(ix, 2)) + (math.pow(y, 2) - math.pow(iy, 2))), 1 / 2)
+         cv2.circle(background, (ix, iy), int(radius), (0, 0, 255), 3)
+
 
 #COMIT ANALTINO
 def main():
@@ -257,6 +312,22 @@ def main():
             rect_pt2_x = int(dot_x)
             rect_pt2_y = int(dot_y)
             cv2.rectangle(background, (rect_pt1_x, rect_pt1_y), (rect_pt2_x, rect_pt2_y), pen_color, cv2.FILLED)
+
+        # draw a rectangle------------------------------------------ INCOMPLETE, DRAW MULTIPLE RECTANGLES
+        if k == ord("s"):
+
+            cv2.namedWindow('Image_Window')
+            rectangle = partial(shape, mode=str('rectangle'))
+            cv2.setMouseCallback('Image_Window',rectangle)
+            cv2.imshow('Image_Window',background)
+
+        # draw a circle------------------------------------------ INCOMPLETE, DOESN'T WORK PROPERLY
+        if k == ord("e"):
+
+            cv2.namedWindow('Image_Window')
+            circle=partial(shape,mode=str('circle'))
+            cv2.setMouseCallback('Image_Window',circle)
+            cv2.imshow('Image_Window',background)
 
         if k == ord("L") and rect_drawing:
             rect_pt2_x = int(dot_x)
